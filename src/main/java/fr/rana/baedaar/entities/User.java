a32706912ac92c0295db4cd1477d5f426e08ca13
Baedaar <baedaarrana2895@gmail;com>
2024-10-21 17:52:09 +0200

    completion of User entity

--- src/main/java/fr/rana/baedaar/entities/User.java
+++ src/main/java/fr/rana/baedaar/entities/User.java
@@ -1,13 +1,40 @@
 package fr.rana.baedaar.entities;
 
+import java.util.List;
+
 public class User {
 
     private String userName;
     private String password;
+    private List<Post> posts;
+    private List<Like> likes;
 
     public User(String userName, String password) {
         this.userName = userName;
         this.password = password;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Like> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Like> likes) {
+        this.likes = likes;
+    }
+
+    public User(String userName, String password, List<Post> posts, List<Like> likes) {
+        this.userName = userName;
+        this.password = password;
+        this.posts = posts;
+        this.likes = likes;
     }
 
     public String getUserName() {
