e139e460d28a0cf8e1cf69910e228cd29e1f79b8
Baedaar <baedaarrana2895@gmail;com>
2024-10-21 17:55:47 +0200

    completion of Like entity

--- src/main/java/fr/rana/baedaar/entities/Like.java
+++ src/main/java/fr/rana/baedaar/entities/Like.java
@@ -1,4 +1,48 @@
 package fr.rana.baedaar.entities;
 
 public class Like {
+
+    private User user;
+    private Comment comment;
+    private Post post;
+
+    public Like(User user, Comment comment) {
+        this.user = user;
+        this.comment = comment;
+    }
+
+    public Like(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
+
+    public Like(User user, Comment comment, Post post) {
+        this.user = user;
+        this.comment = comment;
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
